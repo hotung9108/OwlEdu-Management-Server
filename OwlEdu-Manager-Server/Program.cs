e72426a97a46f745a9ac1de7300e97031918abc1
HoTung <hoviettung2@gmail.com>
2025-11-11 00:01:07 +0700

    add: serviceInjection

--- OwlEdu-Manager-Server/Program.cs
+++ OwlEdu-Manager-Server/Program.cs
@@ -23,27 +23,17 @@ options.UseSqlServer(sqlServerConnection));
 //    options.UseSqlServer(sqlServerConnection));
 //builder.Services.AddScoped<AccountService>();
 //builder.Services.AddScoped<StudentService>();
-builder.Services.AddScoped<StudentService>();
-builder.Services.AddScoped<TeacherService>();
-builder.Services.AddScoped<AccountService>();
+builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ OwlEdu-Manager-Server/Services/ServiceInjection.cs
@@ -0,0 +1,21 @@
+﻿namespace OwlEdu_Manager_Server.Services
+{
+    public static class ServiceInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<StudentService>();
+            services.AddScoped<AccountService>();
+            services.AddScoped<AttendanceService>();
+            services.AddScoped<ClassAssignmentService>();
+            services.AddScoped<CourseService>();
+            services.AddScoped<ClassService>();
+            services.AddScoped<EnrollmentService>();
+            services.AddScoped<PaymentService>();
+            services.AddScoped<ScheduleService>();
+            services.AddScoped<ScoreService>();
+            services.AddScoped<ScoreService>();
+            return services;
+        }
+    }
+}
