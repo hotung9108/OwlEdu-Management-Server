2c8c358ac267fc86ca7d9bd8bbbf54ab50ec54e7
tuanpham936 <tuan.phamminh9936@gmail.com>
2025-11-11 23:39:36 +0700

    add: models DTO

---
+++ OwlEdu-Manager-Server/DTOs/AttendanceDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace OwlEdu_Manager_Server.DTOs
+{
+    public class AttendanceDTO
+    {
+        public string StudentId { get; set; } = null!;
+
+        public string ScheduleId { get; set; } = null!;
+
+        public string? Status { get; set; }
+
+        public string? Note { get; set; }
+
+        public string? TeacherId { get; set; }
+    }
+}
---
+++ OwlEdu-Manager-Server/DTOs/ScoreDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace OwlEdu_Manager_Server.DTOs
+{
+    public class ScoreDTO
+    {
+        public string? Title { get; set; }
+
+        public string StudentId { get; set; } = null!;
+
+        public string ClassId { get; set; } = null!;
+
+        public string? TeacherId { get; set; }
+
+        public decimal? Lisening { get; set; }
+
+        public decimal? Speaking { get; set; }
+
+        public decimal? Reading { get; set; }
+
+        public decimal? Writing { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
