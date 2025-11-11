96281c24ef766ee346b1186fd9efa076804498a7
HoTung <hoviettung2@gmail.com>
2025-11-11 20:36:18 +0700

    add: DTOs

--- OwlEdu-Manager-Server/Controllers/TeacherController.cs
+++ OwlEdu-Manager-Server/Controllers/TeacherController.cs
@@ -1,6 +1,10 @@
-﻿namespace OwlEdu_Manager_Server.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OwlEdu_Manager_Server.Controllers
 {
-    public class TeacherController
+    public class TeacherController : Controller
     {
+
     }
 }
