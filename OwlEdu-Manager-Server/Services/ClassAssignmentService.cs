d66af78ee4ea91b46831934293f0e9b5f5b9516f
HoTung <hoviettung2@gmail.com>
2025-11-17 12:52:36 +0700

    add: enrollment service

--- OwlEdu-Manager-Server/Services/ClassAssignmentService.cs
+++ OwlEdu-Manager-Server/Services/ClassAssignmentService.cs
@@ -8,10 +8,19 @@ namespace OwlEdu_Manager_Server.Services
         public ClassAssignmentService(EnglishCenterManagementContext context) : base(context)
         {
         }
-
         public async Task<ClassAssignment> GetClassAssignmentByClassIdStudentId(string classId, string studentId)
         {
             return await _dbSet.AsNoTracking().Where(t => t.ClassId == classId && t.StudentId == studentId).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<ClassAssignment>> GetClassAssignmentByClassId(string classId)
+        {
+            return await _dbSet.AsNoTracking().Where(t => t.ClassId == classId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<ClassAssignment>> GetClassAssignmentByStudentId(string studentId)
+        {
+            return await _dbSet.AsNoTracking().Where(t => t.StudentId == studentId).ToListAsync();
         }
     }
 }
